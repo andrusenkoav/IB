c2e23b81d0d524cef2a0ef8bce349f27fd667793
alex <alexsey_sar@mail.ru>
2018-11-11 01:32:23 +0300

    ws security test

--- ws-security/src/test/java/ru/ibank/ws/security/ServerPasswordCallbackTest.java
+++ ws-security/src/test/java/ru/ibank/ws/security/ServerPasswordCallbackTest.java
@@ -1,24 +1,43 @@
 package ru.ibank.ws.security;
 
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+import org.junit.Before;
+
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 public class ServerPasswordCallbackTest {
 
-    private static ClassPathXmlApplicationContext ctx;
+    private ServerPasswordCallback serverPasswordCallback;
+    private final Callback[] callbacks = new Callback[1];
+    private final WSPasswordCallback wsPasswordCallback = new WSPasswordCallback("joe",0);
 
-    public static void init (){
-
-        ctx = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/beans.xml",
-                                                              "META-INF/spring/ref-beans.xml",});
-
+    @Before
+    public void init (){
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/beans.xml",
+                                                                                             "META-INF/spring/reference-beans.xml"});
+        serverPasswordCallback = ctx.getBean("passwordCallback", ServerPasswordCallback.class);
+        callbacks[0] = wsPasswordCallback;
     }
 
     @Test
     public void handle() {
 
+        try {
+            serverPasswordCallback.handle(callbacks);
+            assertEquals("123", wsPasswordCallback.getPassword());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (UnsupportedCallbackException e) {
+            e.printStackTrace();
+        }
 
     }
 }
